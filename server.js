64b582aa59f7485c2f5a72bc7d8a9fb38958a7b2
aaaafireball <watsonc5251@gmail.com>
2022-11-04 23:40:47 -0400

    Finished backend server.js file

--- server.js
+++ server.js
@@ -1,45 +1,28 @@
 const http = require('http');
 const fs = require('fs');
-let files = ["errorFile.txt", "warningFile.txt"];
 
 
-function logErrorToFile(res, data) {
-    fs.appendFile(files[0], data + "\n", (error) => {
+
+function logToFile(res, data) {
+    fs.appendFile("logs.txt", data + "\n", (error) => {
         if (error) {
             res.statusCode = 500;
             res.write("Server error when logging to file");
             res.end();
             return;
         }
-        console.log("Appended error to file")
-        res.write("Error logged");
-        res.end();
-    })
-
-
-}
-
-function logWarningToFile(res, data) {
-    fs.appendFile(files[1], data + "\n", (error) => {
-        if (error) {
-            res.statusCode = 500;
-            res.write("Server error when logging to file");
-            res.end();
-            return;
-        }
-        console.log("Appended warning to file")
-        res.write("Warning logged");
+        console.log("Appended to file")
+        res.write("Logged");
         res.end();
     })
 }
 
-function getErrorOrWarning(res, selection){
-    let file = files[selection];
-    fs.readFile(file, (error, data) => {
+function getErrorOrWarningOrAll(res, selection){
+    fs.readFile("logs.txt", (error, data) => {
         if (error) {
             res.statusCode = 500;
             if (error.code === "ENOENT"){
-                res.write(`No ${file} file found`);
+                res.write(`File not found`);
                 res.end();
                 return;
             }
@@ -47,31 +30,25 @@ function getErrorOrWarning(res, selection){
             res.end();
             return;
         }
+        let fileData = data.toString().split("\n");
+
+        if (0 === selection){
+            fileData.forEach((line) => {
+                if (line.includes("Error")) res.write(line + "\n");
+            })
+            res.end();
+            return;
+        }
+        else if (1 === selection){
+            fileData.forEach((line) => {
+                if (line.includes("Warning")) res.write(line + "\n");
+            })
+            res.end();
+            return;
+        }
         res.write(data);
         res.end();
     });
-}
-
-function getAll(res){
-    files.forEach((file) => {
-        console.log(file);
-        fs.readFile(file, (error, data) => {
-            if (error) {
-                res.statusCode = 500;
-                if (error.code === "ENOENT"){
-                    res.write(`No ${file} file found`);
-                    res.end();
-                    return;
-                }
-                res.write("Server error when reading files");
-                res.end();
-                return;
-            }
-            console.log(data);
-            res.write(data);
-
-        });
-    })
 }
 
 
@@ -84,19 +61,19 @@ const server = http.createServer((req, res) => {
         res.setHeader('Content-Type', 'text/plain');
         try{
             if(url === '/log_error' && method === 'GET') {
-                logErrorToFile(res, "Error occurred at: " + Date());
+                logToFile(res, "Error occurred at: " + Date());
             }
             else if (url === '/log_warning' && method === 'GET') {
-                logWarningToFile(res, "Warning occurred at: " + Date());
+                logToFile(res, "Warning occurred at: " + Date());
             }
             else if(url === '/errors' && method === 'GET') {
-                getErrorOrWarning(res, 0);
+                getErrorOrWarningOrAll(res, 0);
             }
             else if(url === '/warnings' && method === 'GET') {
-                getErrorOrWarning(res, 1);
+                getErrorOrWarningOrAll(res, 1);
             }
             else if(url === '/all' && method === 'GET') {
-                getAll(res);
+                getErrorOrWarningOrAll(res,2);
             }
             else {
                 res.statusCode = 404;
@@ -113,7 +90,6 @@ const server = http.createServer((req, res) => {
 
     }
 )
-
 
 server.listen(port, hostname, () => {
         console.log(`Server running at http://${hostname}:${port}/`);
